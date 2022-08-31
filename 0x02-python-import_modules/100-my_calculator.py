eb267a6e8e0a26de904ac04e72355a53b71c8e28
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-09-01 00:01:42 +0200

    feat: added advanced exercises

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    av = sys.argv
+    ac = len(av)
+    if ac != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(av[1])
+    operator = av[2]
+    b = int(av[3])
+
+    if operator is '+':
+        print("{} {} {} = {}".format(a, operator, b, add(a, b)))
+    elif operator is '/':
+        print("{} {} {} = {}".format(a, operator, b, div(a, b)))
+    elif operator is '*':
+        print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
+    elif operator is '-':
+        print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if a >= b:
+        return sub(a, b)
+    else:
+        res = add(a, b)
+        for i in range(4, 6):
+            res = add(res, i)
+        return res
