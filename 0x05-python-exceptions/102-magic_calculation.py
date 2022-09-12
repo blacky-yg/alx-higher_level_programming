05781c0b823aa0c78a087fb8ab1b53511ad4d7bc
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-09-12 19:18:23 +0200

    updatee

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,10 +3,10 @@ def magic_calculation(a, b):
     res = 0
     for i in range(1, 3):
         try:
-            if (i > a):
-                raise Exception('Too far')
-            else:
+            if i <= a:
                 res += a ** b / i
+            else:
+                raise Exception('Too far')
         except Exception:
             res = b + a
             break
