dbaf71d6747cea77a1ee591264cb57fd53b1787a
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-09-12 19:01:20 +0200

    fix: printing format

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     nb_elem = 0
     for i in range(0, x):
         try:
-            print(my_list[i], end="")
+            print("{:d}".format(my_list[i]), end="")
             nb_elem += 1
         except (ValueError, TypeError):
             continue
