27b94171249f90a767063329cf4a23b1672fe6d4
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-09-22 00:08:21 +0200

    feat: more classes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Define a Rectangle class."""
+
+
+class Rectangle:
+    """Representation of a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialization of a new Rectangle.
+
+        Args:
+            width (int): Width of the rectangle.
+            height (int): Height of the rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter and setter of the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter and setter of the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
