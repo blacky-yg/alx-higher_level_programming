767a8b04fb364cebb2a222432f60678099faba57
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-09-24 02:03:40 +0200

    update

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -23,11 +23,15 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError(
             "Each row of the matrix must have the same size")
-   if not all(len(row) == len(matrix[0]) for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-    if not all(type(num) in [int, float] for row in matrix for num in row):
-        raise TypeError("matrix must be a matrix (list of lists)"
-                        " of integers/floats")
-    new_matrix = [[eval("{:.2f}".format(num / div)) for num in row]
-                  for row in matrix]
-    return new_matrix
+    matrix_divided = [x[:] for x in matrix]
+    for line in matrix_divided:
+        if len(line) != len(matrix_divided[0]):
+            raise TypeError('Each row of the matrix must have the same size')
+        for i, elem in enumerate(line):
+            if not isinstance(elem, (int, float)):
+                raise TypeError(
+                    'matrix must be a matrix (list of lists)'
+                    ' of integers/floats'
+                )
+            line[i] = round(elem/div, 2)
+    return matrix_divided
