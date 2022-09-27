46c23fd543b3fff973e102770099b5898d17deee
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-09-27 15:59:49 +0200

    update size validator in square class

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -14,6 +14,7 @@ class Square(Rectangle):
         Args:
             size (int): Size of the square
         """
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
