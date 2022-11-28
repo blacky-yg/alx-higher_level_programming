6462171d5a418fb1145559e682c7a8dab15ccea5
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-11-28 17:22:21 +0100

    update

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const arg = parseInt(process.argv[2]);
 if (isNaN(arg)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + convertArg);
+  console.log('My number: ' + arg);
 }
